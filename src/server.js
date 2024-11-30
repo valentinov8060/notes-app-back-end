a06c6ff1e647546ba6e897ff1513ad7d578a490b
valentinov8060 <valentinovbill0@gmail.com>
2024-11-30 22:24:44 +0700

    feat: add eslint

--- src/server.js
+++ src/server.js
@@ -2,22 +2,22 @@ const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
 
 const init = async () => {
-     const server = Hapi.server({
-          port: 5000,
-          host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
-          // same-origin policy
-          routes: {
-               cors: {
-                    origin: ['*'],
-               },
-          },
-     });
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+    // same-origin policy
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
 
-     // Menambahkan rute
-     server.route(routes);
+  // Menambahkan rute
+  server.route(routes);
 
-     await server.start();
-     console.log(`Server berjalan pada ${server.info.uri}`);
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
 init();
